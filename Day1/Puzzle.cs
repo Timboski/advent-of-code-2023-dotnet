3450017bfb5011d88ca2a10d67addb5f88d4c735
timboski <timboboyle@yahoo.co.uk>
2023-12-02 22:37:44 +0000

    Add test for part 2

--- Day1/Puzzle.cs
+++ Day1/Puzzle.cs
@@ -3,4 +3,6 @@
 public static class Puzzle
 {
     public static int Run(string filename) => File.ReadAllLines(filename).Select(ExtractNumberFromString.ExtractCalibration).Sum();
+
+    public static int RunPart2(string filename) => throw new NotImplementedException();
 }
--- Day1Test/PuzzleTest.cs
+++ Day1Test/PuzzleTest.cs
@@ -5,12 +5,26 @@ public class PuzzleTest
     [Theory]
     [InlineData("day1-example-input.txt", 142)]
     [InlineData("day1-input.txt", 52974)]
-    public void GivenExampleInput_WhenRunProgram_ReturnsExampleResult(string filename, int expected)
+    public void GivenExampleInput_WhenRun_ReturnsExampleResult(string filename, int expected)
     {
         // Arrange - see inline data
 
         // Act
         var actual = Puzzle.Run(filename);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("day1-example-input-part2.txt", 142)]
+    // [InlineData("day1-input.txt", ??)]
+    public void GivenExampleInput_WhenRunPart2_ReturnsExampleResult(string filename, int expected)
+    {
+        // Arrange - see inline data
+
+        // Act
+        var actual = Puzzle.RunPart2(filename);
 
         // Assert
         Assert.Equal(expected, actual);
