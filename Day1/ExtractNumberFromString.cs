6fb9af44d96ba6a99ab266a71494175a4b77e0c4
timboski <timboboyle@yahoo.co.uk>
2023-12-02 22:01:38 +0000

    Refactor char to int

--- Day1/ExtractNumberFromString.cs
+++ Day1/ExtractNumberFromString.cs
@@ -3,7 +3,9 @@ namespace Day1;
 
 public static class ExtractNumberFromString
 {
-    public static int FindFirstNumber(this string input) => int.Parse(input.First(char.IsDigit).ToString());
+    public static int FindFirstNumber(this string input) => input.First(char.IsDigit).CharToInt();
 
-    public static int FindLastNumber(string input) => int.Parse(input.Last(char.IsDigit).ToString());
+    public static int FindLastNumber(this string input) => input.Last(char.IsDigit).CharToInt();
+
+    private static int CharToInt(this char c) => int.Parse(c.ToString());
 }
