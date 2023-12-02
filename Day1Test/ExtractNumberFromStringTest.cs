c8005d77e0a6957dc85474e8cf8423714f37270c
timboski <timboboyle@yahoo.co.uk>
2023-12-02 21:42:33 +0000

    Parameterise test and add example data

--- Day1Test/ExtractNumberFromStringTest.cs
+++ Day1Test/ExtractNumberFromStringTest.cs
@@ -2,12 +2,15 @@ namespace Day1Test;
 
 public class ExtractNumberFromStringTest
 {
-    [Fact]
-    public void GivenStringContainingNumber_WhenFindFirstNumber_ReturnsExpectedNumber()
+    [Theory]
+    [InlineData("fsfd1ljkjl3er", 1)]
+    [InlineData("1abc2", 2)]
+    [InlineData("pqr3stu8vwx", 3)]
+    [InlineData("a1b2c3d4e5f", 1)]
+    [InlineData("treb7uchet", 7)]
+    public void GivenStringContainingNumber_WhenFindFirstNumber_ReturnsExpectedNumber(string input, int expected)
     {
-        // Arrange
-        string input = "fsfd1ljkjl3er";
-        int expected = 1;
+        // Arrange - see inline data
 
         // Act
         var actual = ExtractNumberFromString.FindFirstNumber(input);
