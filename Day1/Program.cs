2707e275dd231813b12ead4a4e743d49b0d15ef3
timboski <timboboyle@yahoo.co.uk>
2023-12-03 18:12:28 +0000

    Attept solve: your answer is too high

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Day1;
 
-Console.WriteLine("Sample Data");
-int sampleResult = Puzzle.Run("day1-input.txt");
-Console.WriteLine(sampleResult);
+Console.WriteLine("Day 1");
+
+Console.Write("Part 1: ");
+int result = Puzzle.Run("day1-input.txt");
+Console.WriteLine(result);
 
 
+Console.Write("Part 2: ");
+int result2 = Puzzle.RunPart2("day1-input.txt");
+Console.WriteLine(result2);
