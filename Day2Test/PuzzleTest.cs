5da687429f9f4ad388119ad6a47e3ce404468916
timboski <timboboyle@yahoo.co.uk>
2023-12-03 23:07:07 +0000

    Solve day 2 part 2

--- Day2Test/PuzzleTest.cs
+++ Day2Test/PuzzleTest.cs
@@ -18,7 +18,7 @@ public class PuzzleTest
 
     [Theory]
     [InlineData("day2-example-input.txt", 2286)]
-    //[InlineData("day2-input.txt", ??)]
+    [InlineData("day2-input.txt", 66027)]
     public void GivenExampleInput_WhenRunPart2_ReturnsExampleResult(string filename, int expected)
     {
         // Arrange - see inline data
