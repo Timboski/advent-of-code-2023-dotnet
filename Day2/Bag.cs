271a7348c03e57bb568574f874c1bf81f3cfa423
timboski <timboboyle@yahoo.co.uk>
2023-12-03 21:34:26 +0000

    Check cubes against bad contents

--- Day2/Bag.cs
+++ Day2/Bag.cs
@@ -1,8 +1,18 @@
-﻿namespace Day2;
+﻿
+namespace Day2;
 
 public class Bag(int _numRed, int _numGreen, int _numBlue)
 {
     public int NumRed => _numRed;
     public int NumGreen => _numGreen;
     public int NumBlue => _numBlue;
+
+    public bool IsValid(string colour, int count) =>
+        colour switch
+        {
+            "red" => count <= _numRed,
+            "green" => count <= _numGreen,
+            "blue" => count <= _numBlue,
+            _ => throw new NotImplementedException(),
+        };
 }
--- Day2Test/BagTest.cs
+++ Day2Test/BagTest.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace Day2Test;
 
 public class BagTest
@@ -17,5 +20,27 @@ public class BagTest
         Assert.Equal(numRed, sut.NumRed);
         Assert.Equal(numGreen, sut.NumGreen);
         Assert.Equal(numBlue, sut.NumBlue);
+    }
+
+    [Theory]
+    [InlineData("green", 13, true)]
+    [InlineData("red", 13, false)]
+    [InlineData("green", 14, false)]
+    [InlineData("red", 12, true)]
+    [InlineData("red", 1, true)]
+    [InlineData("blue", 0, true)]
+    [InlineData("blue", 14, true)]
+    [InlineData("blue", 15, false)]
+    [InlineData("blue", 255, false)]
+    public void GivenNumberOfColouredCubes_WhenCheckIsValid_ReturnsWhetherCubesInBag(string colour, int count, bool expectedResult)
+    {
+        // Arrange
+        var sut = new Bag(_numRed: 12, _numGreen: 13, _numBlue: 14);
+
+        // Act
+        var result = sut.IsValid(colour, count);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
     }
 }
